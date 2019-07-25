d1f22f13f2f2a0d600ba5e58c7535c8ebfebe61e
yspolat <yspolat@gmail.com>
2019-07-25 21:37:48 +0300

    Initial
    Initial commit

---
+++ pharmacy-spring/src/main/java/com/it529/teamgy/pharmacy/pharmacyspring/PharmacySpringApplication.java
@@ -0,0 +1,13 @@
+package com.it529.teamgy.pharmacy.pharmacyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacySpringApplication.class, args);
+	}
+
+}
